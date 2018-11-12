741122158cdbf0ae5fa801d80b80877b89650a61
JTLavs <jamestlavery@gmail.com>
2018-11-12 20:14:04 +0000

    Added inital styling for side menu

--- Menu.js
+++ Menu.js
@@ -6,6 +6,7 @@ import {
   ScrollView,
   View,
   Text,
+  Image
 } from 'react-native';
 
 const window = Dimensions.get('window');
@@ -13,55 +14,77 @@ const window = Dimensions.get('window');
 const styles = StyleSheet.create({
   menu: {
     flex: 1,
-    width: window.width,
+    width: window.width / 1.5,
     height: window.height,
     backgroundColor: 'white',
-    padding: 20,
-  },
-  avatarContainer: {
-    marginBottom: 20,
-    marginTop: 20,
-  },
-  avatar: {
-    width: 48,
-    height: 48,
-    borderRadius: 24,
-    flex: 1,
+    borderRightColor:'black',
+    borderRightWidth:1,
+    borderStyle:'solid',
   },
   name: {
-    position: 'absolute',
-    left: 70,
-    top: 20,
+    fontSize:30,
+    marginTop: 30,
+    padding:10,
+  },
+  header_view:{
+      alignItems:'center'
   },
   item: {
-    fontSize: 15,
+    fontSize: 20,
+    fontWeight: 'bold',
     fontWeight: '300',
-    paddingTop: 5,
-    marginTop:10
+    width:'100%',
+    padding:10,
+    marginTop:5,
+    color:'green',
+    backgroundColor:'lightgray'
+  },
+  profile_image: {
+    height:100,
+    width:100,
+    borderRadius:50
   },
 });
 
 export default function Menu({ onItemSelected }) {
   return (
-    <ScrollView scrollsToTop={false} style={styles.menu}>
-      <View style={styles.avatarContainer}>
+    <View scrollsToTop={false} style={styles.menu}>
+      <View style={styles.header_view}>
         <Text style={styles.name}>Hello James</Text>
-      </View>
-
-      <Text
-        onPress={() => onItemSelected('About')}
-        style={styles.item}
-      >
-        About
-      </Text>
-
+      <Image 
+        source={{uri : "https://facebook.github.io/react-native/docs/assets/favicon.png"}}     
+        style={styles.profile_image}/>
       <Text
         onPress={() => onItemSelected('Contacts')}
-        style={styles.item}
-      >
-        Contacts
+        style={styles.item}>
+        Homework
       </Text>
-    </ScrollView>
+    </View>
+
+    <View>        
+      <Text
+        onPress={() => onItemSelected('About')}
+        style={styles.item}>
+        Timetable
+      </Text>
+      </View>
+
+    <View>
+      <Text
+        onPress={() => onItemSelected('Contacts')}
+        style={styles.item}>
+        Exams
+      </Text>
+    </View>
+
+    <View>
+      <Text
+        onPress={() => onItemSelected('Contacts')}
+        style={styles.item}>
+        Clubs and Societies
+      </Text>
+    </View>
+    </View>
   );
 }
 
